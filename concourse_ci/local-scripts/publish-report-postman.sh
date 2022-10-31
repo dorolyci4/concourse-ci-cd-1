e2dddd3d40ad72ea413f6491b7005b63b27fefaf
Alexis <dorolyci4@gmail.com>
2022-10-30 22:38:15 -0400

    test pour newman

--- concourse_ci/local-scripts/publish-report-postman.sh
+++ concourse_ci/local-scripts/publish-report-postman.sh
@@ -7,6 +7,11 @@ if [ -z "$COLLECTION_URL" ]; then
   erreur_validation=1
 fi
 
+if [ -z "$ENVIRONMENT_URL" ]; then
+  echo -e "Le paramètre ENVIRONMENT_URL doit être renseigné."
+  erreur_validation=1
+fi
+
 if [ -z "$erreur_validation" == "1" ]; then
   echo -e "ARRÊT DU TRAITEMENT."
   exit 1
@@ -19,6 +24,7 @@ echo " Newman Version: "$(newman --version)
 
 echo "Test Environment"
 echo " Collection URL: " $COLLECTION_URL
+echo " ENVIRONMENT URL: " ENVIRONMENT_URL
 
 # Fonction qui permet de valider et de définir le statut de la job Concourse (à appeler à la fin du script)
 function setExitStatus {
@@ -41,7 +47,7 @@ npm i -g newman-reporter-htmlextra
 
 echo "run newman"
 echo $COLLECTION_URL
-newman run $COLLECTION_URL -r htmlextra
+newman run $COLLECTION_URL -e $ENVIRONMENT_URL -r htmlextra
 #newman run $COLLECTION_URL \
 #       --color off
 #       --reporters cli,html \
